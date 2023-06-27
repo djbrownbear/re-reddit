130d43ea59552b21ddafc64f995eeae3e5d08b3f
Aaron Brown <41420965+djbrownbear@users.noreply.github.com>
2023-06-27 16:42:47 -0700

    fix: display UserMenu

--- src/components/Navbar.tsx/RightContent/UserMenu.tsx
+++ src/components/Navbar.tsx/RightContent/UserMenu.tsx
@@ -1,48 +1,80 @@
 import { ChevronDownIcon } from "@chakra-ui/icons";
-import { Button, Flex, Icon, Menu, MenuButton, MenuDivider, MenuItem, MenuList } from "@chakra-ui/react";
+import {
+  Button,
+  Flex,
+  Icon,
+  Menu,
+  MenuButton,
+  MenuDivider,
+  MenuItem,
+  MenuList,
+} from "@chakra-ui/react";
 import React from "react";
 import { FaRedditSquare } from "react-icons/fa";
 import { VscAccount } from "react-icons/vsc";
 import { IoSparkles } from "react-icons/io5";
 import { CgProfile } from "react-icons/cg";
 import { MdOutlineLogin } from "react-icons/md";
+import { signOut, User } from "firebase/auth";
+import { auth } from "@/firebase/clientApp";
 
 type UserMenuProps = {
   user?: User | null;
 };
 
 const UserMenu: React.FC<UserMenuProps> = ({ user }) => {
-  <Menu>
-    <MenuButton cursor="pointer" padding="0px 6px" borderRadius={4} _hover={{ outline: "1px solid", outlineColor: "gray.200" }}>
-      {user ? (
+  return (
+    <Menu>
+      <MenuButton
+        cursor="pointer"
+        padding="0px 6px"
+        borderRadius={4}
+        _hover={{ outline: "1px solid", outlineColor: "gray.200" }}
+      >
         <Flex align="center">
           <Flex align="center">
-            <>
-              <Icon fontSize={24} mr={1} color="gray.300" as={FaRedditSquare} />
-            </>
+            {user ? (
+              <>
+                <Icon
+                  fontSize={24}
+                  mr={1}
+                  color="gray.300"
+                  as={FaRedditSquare}
+                />
+              </>
+            ) : (
+              <Icon fontSize={24} color="gray.400" mr={1} as={VscAccount} />
+            )}
             <ChevronDownIcon />
           </Flex>
         </Flex>
-      ) : (
-        <Icon fontSize={24} color="gray.400" mr={1} as={VscAccount} />
-      )}
-    </MenuButton>
-    <MenuList>
-      <MenuItem fontSize="10pt" fontWeight={700} _hover={{ bg: "blue.500", color: "white" }}>
-        <Flex align="center">
-          <Icon fontSize={20} mr={2} as={CgProfile} />
-          Profile
-        </Flex>
-      </MenuItem>
-      <MenuDivider />
-      <MenuItem fontSize="10pt" fontWeight={700} _hover={{ bg: "blue.500", color: "white" }}>
-        <Flex align="center">
-          <Icon fontSize={20} mr={2} as={MdOutlineLogin} />
-          Log Out
-        </Flex>
-      </MenuItem>
-    </MenuList>
-  </Menu>;
+      </MenuButton>
+      <MenuList>
+        <MenuItem
+          fontSize="10pt"
+          fontWeight={700}
+          _hover={{ bg: "blue.500", color: "white" }}
+        >
+          <Flex align="center">
+            <Icon fontSize={20} mr={2} as={CgProfile} />
+            Profile
+          </Flex>
+        </MenuItem>
+        <MenuDivider />
+        <MenuItem
+          fontSize="10pt"
+          fontWeight={700}
+          _hover={{ bg: "blue.500", color: "white" }}
+          onClick={() => signOut(auth)}
+        >
+          <Flex align="center">
+            <Icon fontSize={20} mr={2} as={MdOutlineLogin} />
+            Log Out
+          </Flex>
+        </MenuItem>
+      </MenuList>
+    </Menu>
+  );
 };
 
 export default UserMenu;
